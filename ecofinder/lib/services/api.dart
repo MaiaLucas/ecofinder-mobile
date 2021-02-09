82b5c5107ed6779bba05e25f38b3f6ef8f5b7606
MaiaLucas <barretomaia5466@outlook.com>
2021-02-08 23:00:21 -0300

    feat: requisição de login do usuário

--- ecofinder/lib/services/api.dart
+++ ecofinder/lib/services/api.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'package:ecofinder/models/login_model.dart';
 import 'package:http/http.dart' as http;
 
 class URLS {
-  static const String BASE_URL = 'https://ecofinder-api.herokuapp.com';
+  static const String BASE_URL = 'http://192.168.0.45:8888';
+  //'https://ecofinder-api.herokuapp.com';
 }
 
 class ApiService {
@@ -12,6 +14,31 @@ class ApiService {
       return json.decode(response.body);
     } else {
       return null;
+    }
+  }
+
+  static Future<LoginResponseModel> signIn(
+      String email, String password) async {
+    final response = await http.post("${URLS.BASE_URL}/signin",
+        headers: {"content-type": "application/json"},
+        body: jsonEncode({"email": email, "password": password}));
+
+    switch (response.statusCode) {
+      case 200:
+        print(json.decode(response.body));
+        return LoginResponseModel.fromJson(
+        json.decode(response.body),
+      );
+        break;
+      case 400:
+      case 401:
+        print(json.decode(response.body));
+        return LoginResponseModel.fromJson(
+        json.decode(response.body),
+      );
+        break;
+      default:
+        print('error');
     }
   }
 }

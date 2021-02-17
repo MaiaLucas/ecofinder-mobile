e7a106939505d269a5b62b6ec29cabd049f500a1
MaiaLucas <barretomaia5466@outlook.com>
2021-02-17 18:22:44 -0300

    feat: integrando a dashboard com a API

--- ecofinder/lib/services/api.dart
+++ ecofinder/lib/services/api.dart
@@ -16,6 +16,8 @@ class ApiService {
     }
   }
 
+  // LOGIN
+
   static Future<dynamic> signIn(data) async {
     final response = await http.post(
       "${URLS.BASE_URL}/signin",
@@ -26,6 +28,8 @@ class ApiService {
     return response;
   }
 
+  //CADASTRO
+
   static Future<dynamic> signUp(data) async {
     final response = await http.post(
       "${URLS.BASE_URL}/signup",
@@ -34,5 +38,16 @@ class ApiService {
     );
 
     return response;
+  }
+
+  //CARREGA AS INFORMAÇÕES DO DASHBOARD
+
+  static Future<dynamic> loadInfo() async {
+    final response = await http.get("${URLS.BASE_URL}/place");
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      return null;
+    }
   }
 }

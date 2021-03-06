5dfa4fdbcac63eccb985cf11223a7d8f3f63a3eb
MaiaLucas <barretomaia5466@outlook.com>
2021-03-06 10:18:39 -0300

    conversão de stateless para stateful

--- ecofinder/lib/screens/dashboard/dash_home.dart
+++ ecofinder/lib/screens/dashboard/dash_home.dart
@@ -1,3 +1,4 @@
+import 'package:ecofinder/models/Highlights.dart';
 import 'package:ecofinder/services/api.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -5,15 +6,37 @@ import 'package:flutter/material.dart';
 import '../../components/dashboard/dashboard_home_item.dart';
 import '../../components/dashboard/dashboard_main_item.dart';
 
-class DashHome extends StatelessWidget {
+class DashHome extends StatefulWidget {
+  @override
+  _DashHomeState createState() => _DashHomeState();
+}
+
+class _DashHomeState extends State<DashHome> {
+  Future<Highlights> highlights;
+
+  @override
+  void initState() {
+    super.initState();
+    print('init state');
+    highlights = ApiService.loadDashboardInfo();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: ApiService.loadDashboardInfo(),
       builder: (context, snapshot) {
-        final info = snapshot.data;
-        print('info');
-        print(info);
+        print(snapshot);
+        if (snapshot.hasData) {
+          final info = snapshot.data;
+          print(info);
+        }
+
         return Scaffold(
           body: Column(
             children: [
@@ -79,13 +102,13 @@ class DashHome extends StatelessWidget {
                   children: [
                     DashboardHomeItem(
                       image:
-                          "https://lh3.googleusercontent.com/proxy/O5imHvjxzGtMmG_hNpwQN3Y2jE3BCrmGAnknYrypSsER2DDULh0FiOF3RXVBYua35k4OPIO2ONhQnia2eBhcONxbPHzzZP9TzJ8cEAld1dWc9yuamFQvr8xpmKZ51y4RCByZJWDE3uTorOPYcI5yNukea5CkHoGjRhWdjws6fHw9Lu6ERwDktVhT5ybVrJALwhCGiGk4jIYD2Px6ke953wTEbRPP2dle",
+                          "https://images.pexels.com/photos/4503265/pexels-photo-4503265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
                       name: "Terceiro local de coleta",
                       rating: "4.3",
                     ),
                     DashboardHomeItem(
                       image:
-                          "https://lh3.googleusercontent.com/proxy/_EXf3GIqmucSsLbLkc2K-c6ISqwaTkTKg0Oh4GClqn71zWRdVmg3jDtcitkXWeQaEEnLrDGqeI8MTIbTvhx5erXCpd1Rfv6ITQ8zpC49Np0W0NpjUZwHt-_mIoz0VjkK-V0rJJYSxyI5MMg",
+                          "https://images.pexels.com/photos/4503265/pexels-photo-4503265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
                       name: "Quadragésimo quinto local de coleta",
                       rating: "3.9",
                     ),

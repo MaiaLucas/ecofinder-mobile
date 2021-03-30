ed57a7eb84674ca24a23486a7c9f4ac93dc4998f
MaiaLucas <barretomaia5466@outlook.com>
2021-03-29 22:50:23 -0300

    criação da tela de busca por produto

--- ecofinder/lib/screens/dashboard/dash_store.dart
+++ ecofinder/lib/screens/dashboard/dash_store.dart
@@ -1,8 +1,51 @@
+import 'dart:convert';
+
+import 'package:ecofinder/components/templates/Search.dart';
+import 'package:ecofinder/services/api.dart';
+import 'package:ecofinder/utils/constants.dart';
+import 'package:ecofinder/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class DashStore extends StatelessWidget {
+class DashStore extends StatefulWidget {
+  @override
+  _DashStoreState createState() => _DashStoreState();
+}
+
+class _DashStoreState extends State<DashStore> {
+  final _searchValueController = TextEditingController();
+  List<dynamic> productsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void handleChange() async {
+    final searchValue = _searchValueController.text;
+    final searchList = await ApiService.searchProduct(products: searchValue);
+    setState(() {
+      productsList = searchList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+        backgroundColor: Constants.BACKGROUND,
+        body: SafeArea(
+          child: Search(
+            listOfValues: productsList,
+            search: {
+              "method": handleChange,
+              "controller": _searchValueController
+            },
+            router: Routes.PRODUCTDETAIL,
+          ),
+        ));
   }
 }

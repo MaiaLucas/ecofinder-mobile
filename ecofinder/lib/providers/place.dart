91934cac4d02c86290bb0d0e8ac716a066f78b08
MaiaLucas <barretomaia5466@outlook.com>
2021-05-01 01:20:50 -0300

    adicionando metodo de cadastro de local

--- ecofinder/lib/providers/place.dart
+++ ecofinder/lib/providers/place.dart
@@ -1,18 +1,25 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:ecofinder/services/urls.dart';
+import 'package:ecofinder/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class PlaceProvider with ChangeNotifier {
   Map<String, dynamic> _createPlace = {
-    'title': '',
-    'description': '',
-    'phone': '',
-    'adress': '',
-    'city': '',
-    'open_on_weekend': '',
+    'title': 'a',
+    'type': 1,
+    'description': 'a',
+    'phone': 'a',
+    'address': 'a',
+    'city': 'a',
+    'open_on_weekend': true,
     'hr_init': '',
     'hr_final': '',
   };
 
-  int _currentStep = 0;
+  int _currentStep = 2;
 
   set createPlace(Map<String, dynamic> value) {
     _createPlace = value;
@@ -30,5 +37,36 @@ class PlaceProvider with ChangeNotifier {
 
   get currentStep {
     return _currentStep;
+  }
+
+  Future<void> create(context) async {
+    try {
+      final response = await http.post(
+        "${URLS.BASE_URL}/place",
+        body: jsonEncode(_createPlace),
+        headers: {"Content-Type": "application/json"},
+      );
+
+      final responseBody = jsonDecode(response.body);
+      if (response.statusCode != 200 && responseBody['error'])
+        throw responseBody['error'];
+
+      Navigator.pushNamed(context, Routes.CONFIRMATION);
+    } catch (e) {
+      Widget snackBar = SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text(
+          e != null
+              ? e
+              : 'Não foi possível cadastrar! Tente novamente mais tarde',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+
+    // print(responseBody);
   }
 }
